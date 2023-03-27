74f7ee8443fa7607ba1db0a9a65d410a9af0f71c
smhrds <smhrds@smhrd>
2023-03-27 16:17:22 +0900

    database

--- mealkittest/src/main/java/domain/ProductDAO.java
+++ mealkittest/src/main/java/domain/ProductDAO.java
@@ -1,5 +1,13 @@
 package domain;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import database.SqlSessionManager;
+
 public class ProductDAO {
 
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSessionFactory();
+	SqlSession sqlSession = sqlSessionFactory.openSession();
+	
 }
