1ba01d3dee7eef77bf2956c11090fc5a0793bc21
smhrd <smhrd@shmrd>
2023-03-27 16:28:59 +0900

    update

--- mealkittest/src/main/java/controller/UpdateUser.java
+++ mealkittest/src/main/java/controller/UpdateUser.java
@@ -6,10 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import domain.User;
+import domain.UserDAO;
+
 public class UpdateUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String pw = request.getParameter("pw");
+		String name = request.getParameter("name");
+		String nick = request.getParameter("nick");
+		char gender = request.getParameter("gender").charAt(0);
+		String addr = request.getParameter("addr");
+		String phone = request.getParameter("phone");
+		
+		
+		User user = new User(pw, name, nick, gender, addr, phone);
+		UserDAO userDao = new UserDAO();
+		
 	}
 
 }
